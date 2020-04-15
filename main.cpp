a42c50bc2122849873245f9b4c37cbeca5e6ce14
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-04-15 17:52:25 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <vector>
+#include <fstream>
+#include <filesystem>
+
+using matrix = std::vector<std::vector<double>>;
+namespace fs = std::filesystem;
+
+matrix parse_csv(const fs::path &input_csv_file) {
+    std::ifstream data(input_csv_file);
+    std::string line;
+    // Skipping required dimension lines
+    std::getline(data, line);
+    matrix parsed_csv;
+    while (std::getline(data, line)) {
+        std::vector<double> parsedRow;
+        std::stringstream s(line);
+        std::string cell;
+        while (std::getline(s, cell, ';')) {
+            parsedRow.push_back(std::stod(cell));
+        }
+
+        parsed_csv.push_back(parsedRow);
+    }
+    return parsed_csv;
+};
