49999b230e512c7e3c65885d49a75503a21c1b62
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-04-17 11:59:45 +0200

    Added algorithm for gauss which seems to work.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <filesystem>
+#include <iterator>
 
 const int DECIMAL_PRECISION = 6;
 using matrix = std::vector<std::vector<double>>;
@@ -25,3 +26,70 @@ matrix parse_csv(const fs::path &input_csv_file) {
     }
     return parsed_csv;
 };
+
+void save_matrix(const std::vector<double> &row, const std::string &output_file_path) {
+    std::ofstream output(output_file_path);
+
+    output << row.size() << std::endl;
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(DECIMAL_PRECISION);
+    oss.str(std::string());
+    std::copy(row.begin(), row.end() - 1, std::ostream_iterator<double>(oss, ";"));
+    std::copy(row.end() - 1, row.end(), std::ostream_iterator<double>(oss));
+    output << oss.str() << std::endl;
+}
+
+matrix &first_stage(matrix &c) {
+    double val;
+    int n = c.size();
+    for (int r = 0; r < n - 1; r++) {
+        for (int i = r+1; i < n; i++) {
+            for (int j = r+1; j < n+1; j++) {
+                printf("%f - (%f/%f * %f)\n", c[i][j], c[i][r],c[r][r], c[r][j]);
+                val = c[i][j] - (c[i][r]/c[r][r] * c[r][j]);
+                c[i][j] = val;
+                printf("r: %d, i: %d, j: %d\n", r, i, j);
+                printf("Current value: %f\n", val);
+            }
+        }
+    }
+    return c;
+}
+
+std::vector<double> second_stage(const matrix &c) {
+    int n = c.size();
+    std::vector<double> x(n);
+    double s;
+    x[n - 1] = c[n - 1][n] / c[n - 1][n - 1];
+    for (int i = n-2; i >= 0; i--) {
+        s = 0;
+        for (int r = i; r < n; r++) {
+            s = s + c[i][r] * x[r];
+            printf("%f\n", s);
+        }
+        printf("Output: %f\n", (c[i][n] - s) / c[i][i]);
+        x[i] = (c[i][n] - s) / c[i][i];
+    }
+    return x;
+}
+
+void gauss(matrix c) {
+    c = first_stage(c);
+
+//    for (const std::vector<double>& inner_row: c) {
+//        for (double single_cell : inner_row) {
+//            printf("%f\t", single_cell);
+//        }
+//        printf("\n");
+//    }
+    std::vector<double> x = second_stage(c);
+    save_matrix(x, "out.csv");
+}
+
+
+int main(int argc, char *argv[]) {
+    fs::path input_path(fs::absolute("input3.csv"));
+    matrix input_matrix = parse_csv(input_path);
+    gauss(input_matrix);
+    return 0;
+}
