b063a5d296adeabf2ac80c9183166d5e0205eef3
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-05-25 17:47:56 +0200

    Well it works... sort of.

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,25 @@ namespace datalogger {
     }
 }
 
+std::string set_schedule_type(std::string &chosen_type, int chunk_size) {
+    if (chosen_type == "static") {
+        omp_set_schedule(omp_sched_static, chunk_size);
+    } else if (chosen_type == "dynamic") {
+        omp_set_schedule(omp_sched_dynamic, chunk_size);
+    } else if (chosen_type == "guided") {
+        omp_set_schedule(omp_sched_guided, chunk_size);
+    } else {
+        chosen_type = "auto";
+        omp_set_schedule(omp_sched_auto, chunk_size);
+    }
+    std::ostringstream oss;
+    oss << chosen_type;
+    if (chunk_size > 0 && chosen_type != "auto") {
+        oss << " with " << chunk_size << " chunks";
+    }
+    return oss.str();
+}
+
 matrix load_csv(const fs::path &input_csv_file) {
     std::ifstream data(input_csv_file);
     std::string line;
@@ -73,42 +92,49 @@ void save_matrix(const std::vector<double> &row, const std::string &output_file_
     output << oss.str() << std::endl;
 }
 
-matrix &first_stage(matrix &c, bool parallelize, int processors_count, int chunk_size) {
+matrix first_stage_sequentially(matrix &c) {
     int n = c.size();
     int r, i, j;
-    if (!parallelize) {
-        printf("First stage sequentially\n");
+    printf("First stage sequentially\n");
+    for (r = 0; r < n - 1; r++) {
+        for (i = r + 1; i < n; i++) {
+            for (j = r + 1; j < n + 1; j++) {
+                c[i][j] = c[i][j] - (c[i][r] / c[r][r] * c[r][j]);
+            }
+        }
+    }
+    return c;
+}
+
+std::vector<double> second_stage_sequentially(const matrix &c) {
+    int n = c.size();
+    std::vector<double> x(n);
+    double s = 0.0;
+    int i, r;
+    x[n - 1] = c[n - 1][n] / c[n - 1][n - 1];
+    printf("Second stage sequentially\n");
+    for (i = n - 2; i >= 0; i--) {
+        s = 0;
+        for (r = i; r < n; r++) {
+            s = s + c[i][r] * x[r];
+        }
+        x[i] = (c[i][n] - s) / c[i][i];
+    }
+    return x;
+}
+
+matrix &first_stage(matrix &c, int processors_count, const std::string &schedule_type_info) {
+    int n = c.size();
+    int r, i, j;
+#pragma omp parallel shared(c, n, r, schedule_type_info) num_threads(processors_count) private(i, j) default(none)
+    {
+        printf("First stage using schedule: %s on processor: %d\n", schedule_type_info.c_str(),
+               omp_get_thread_num());
         for (r = 0; r < n - 1; r++) {
+#pragma omp for ordered schedule(static, 4)
             for (i = r + 1; i < n; i++) {
                 for (j = r + 1; j < n + 1; j++) {
                     c[i][j] = c[i][j] - (c[i][r] / c[r][r] * c[r][j]);
-                }
-            }
-        }
-    } else {
-        if (chunk_size != 0) {
-#pragma omp parallel shared(c, n, chunk_size, r, processors_count) num_threads(processors_count) private(i, j) default(none)
-            printf("First stage using static schedule processor number: %d chunks: %d\n",
-                   omp_get_thread_num(), chunk_size);
-            for (r = 0; r < n - 1; r++) {
-#pragma omp for ordered schedule(static, chunk_size)
-                for (i = r + 1; i < n; i++) {
-                    for (j = r + 1; j < n + 1; j++) {
-                        c[i][j] = c[i][j] - (c[i][r] / c[r][r] * c[r][j]);
-                    }
-                }
-            }
-        } else {
-#pragma omp parallel shared(c, n, chunk_size, r, processors_count) num_threads(processors_count) private(i, j) default(none)
-            {
-                printf("First stage using static schedule processor number: %d\n", omp_get_thread_num());
-                for (r = 0; r < n - 1; r++) {
-#pragma omp for ordered schedule(static)
-                    for (i = r + 1; i < n; i++) {
-                        for (j = r + 1; j < n + 1; j++) {
-                            c[i][j] = c[i][j] - (c[i][r] / c[r][r] * c[r][j]);
-                        }
-                    }
                 }
             }
         }
@@ -118,56 +144,34 @@ matrix &first_stage(matrix &c, bool parallelize, int processors_count, int chunk
 
 
 std::vector<double>
-second_stage(const matrix &c, bool parallelize, int processors_count, int chunk_size) {
+second_stage(const matrix &c, int processors_count, const std::string &schedule_type_info) {
     int n = c.size();
     std::vector<double> x(n);
     double s = 0.0;
     int i, r;
     x[n - 1] = c[n - 1][n] / c[n - 1][n - 1];
-    if (!parallelize) {
-        printf("Second stage sequentially\n");
-        for (i = n - 2; i >= 0; i--) {
-            s = 0;
-            for (r = i; r < n; r++) {
-                s = s + c[i][r] * x[r];
-            }
-            x[i] = (c[i][n] - s) / c[i][i];
+    printf("Second stage in parallel using schedule: %s\n", schedule_type_info.c_str());
+    for (i = n - 2; i >= 0; i--) {
+        s = 0.0;
+#pragma omp parallel for shared(c, n, x, i) num_threads(processors_count) private(r) default(none) \
+        schedule(runtime) reduction(+ : s)
+        for (r = i; r < n; r++) {
+            s += c[i][r] * x[r];
         }
-    } else {
-        if (chunk_size != 0) {
-            printf("Second stage using static schedule with %d processors and %d chunks\n",
-                   processors_count, chunk_size);
-            for (i = n - 2; i >= 0; i--) {
-                s = 0.0;
-#pragma omp parallel for shared(c, n, parallelize, x, i) num_threads(processors_count) private(r) default(none) \
-        schedule(static) reduction(+ : s)
-                for (r = i; r < n; r++) {
-                    s += c[i][r] * x[r];
-                }
-                x[i] = (c[i][n] - s) / c[i][i];
-            }
-        } else {
-            printf("Second stage using static schedule with %d processors\n", processors_count);
-
-            for (i = n - 2; i >= 0; i--) {
-                s = 0.0;
-#pragma omp parallel for shared(c, n, parallelize, chunk_size, x, i) num_threads(processors_count) private(r) \
-        default(none) schedule(static) reduction(+ : s)
-                for (r = i; r < n; r++) {
-                    s += c[i][r] * x[r];
-                }
-
-                x[i] = (c[i][n] - s) / c[i][i];
-            }
-        }
+        x[i] = (c[i][n] - s) / c[i][i];
     }
     return x;
 }
 
-std::vector<double> gauss(matrix c, bool parallelize, int processors_count, int chunk_size) {
-
-    c = first_stage(c, parallelize, processors_count, chunk_size);
-    std::vector<double> x = second_stage(c, parallelize, processors_count, chunk_size);
+std::vector<double> gauss(matrix c, bool parallelize, int processors_count, const std::string &schedule_type_info) {
+    std::vector<double> x;
+    if (!parallelize) {
+        c = first_stage_sequentially(c);
+        x = second_stage_sequentially(c);
+    } else {
+        c = first_stage(c, processors_count, schedule_type_info);
+        x = second_stage(c, processors_count, schedule_type_info);
+    }
     return x;
 
 }
@@ -177,14 +181,14 @@ int main(int argc, char *argv[]) {
     bool datalogger_already_created = fs::exists("datalogger.log");
     std::ofstream data_logger_file;
     data_logger_file.open("datalogger.log", std::ios_base::app);
-    if(!datalogger_already_created) {
+    if (!datalogger_already_created) {
         data_logger_file << datalogger::header() << std::endl;
     }
-    std::string prompt;
+    std::string prompt, schedule_type, schedule_type_info;
     prompt = "N";
     int chunk_size, process_count;
     while (prompt == "N") {
-        printf("Provide input file: ");
+        printf("Provide input file:\n");
         std::cin >> prompt;
         fs::path input_path(fs::absolute(prompt));
         if (!fs::exists(input_path)) {
@@ -196,8 +200,11 @@ int main(int argc, char *argv[]) {
         chunk_size = 0;
         printf("Specify number of processors:\n");
         std::cin >> process_count;
-        printf("Specify chunk size for static scheduling (0 = default):\n");
+        printf("Specify schedule type (static, dynamic, guided, auto[default]):\n");
+        std::cin >> schedule_type;
+        printf("Specify chunk size scheduling (0 = default):\n");
         std::cin >> chunk_size;
+        schedule_type_info = set_schedule_type(schedule_type, chunk_size);
         std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
         std::string start_day(30, '\0');
         std::string start_time(30, '\0');
@@ -208,12 +215,12 @@ int main(int argc, char *argv[]) {
         matrix c = load_csv(input_path);
         printf("Calculating\n");
         double execution_start_time = omp_get_wtime();
-        std::vector<double> x_parallelized = gauss(c, true, process_count, chunk_size);
+        std::vector<double> x_parallelized = gauss(c, true, process_count, schedule_type_info);
         double execution_end_time = omp_get_wtime();
         double Tp = execution_end_time - execution_start_time;
         printf("Tp: %f seconds\n", Tp);
         execution_start_time = omp_get_wtime();
-        std::vector<double> x_sequential = gauss(c, false, process_count, chunk_size);
+        std::vector<double> x_sequential = gauss(c, false, process_count, schedule_type_info);
         execution_end_time = omp_get_wtime();
         double Ts = execution_end_time - execution_start_time;
         printf("Ts: %f seconds\n", Ts);
